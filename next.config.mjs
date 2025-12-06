135e6636bcfc03d2a7430ad0d82a4922882f2922
Jorge Sánchez <georgejordy10@gmail.com>
2025-12-05 21:31:44 -0500

    fix: disable eslint and ts errors in build

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // No bloquear el build por errores de ESLint
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // No bloquear el build por errores de TypeScript
     ignoreBuildErrors: true,
   },
 };
