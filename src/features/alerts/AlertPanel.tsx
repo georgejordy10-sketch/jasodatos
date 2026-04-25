e3fd52c066c1241e018f47875b010ac7b9f4e4a6
Jorge Sanchez <georgejordy10@gmail.com>
2026-04-25 10:12:58 -0500

    Corrige typo JSX en panel de alertas

--- src/features/alerts/AlertPanel.tsx
+++ src/features/alerts/AlertPanel.tsx
@@ -150,7 +150,7 @@ export default function AlertPanel({ alerts }: AlertPanelProps) {
 )}
 
               </div>
-            </article>s
+            </article>
           ))}
         </div>
       )}
